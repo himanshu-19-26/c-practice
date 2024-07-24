0ab5dfc95e34ceee600db497e063c472c71a52e7
Himanshu Yadav <iamhimanshu1926>
2024-07-24 16:24:09 +0530

    add

--- tute7.cpp
+++ tute7.cpp
@@ -3,12 +3,22 @@ using namespace std;
 int c=45;
 int main(){
     int a,b,c;
-    cout<<"enter the value of a:"<<endl;
-    cin>>a;
-    cout<<"enter the value of b:"<<endl;
-    cin>>b;
-    c=a+b;
-    cout<<"value of the sum is :"<<c<<endl;
-    cout<<"the global value of c is:"<<::c;
+ // cout<<"enter the value of a:"<<endl;
+   // cin>>a;
+    //cout<<"enter the value of b:"<<endl;
+    //cin>>b;
+    //c=a+b;
+    //cout<<"value of the sum is :"<<c<<endl;
+    //cout<<"the global value of c is:"<<::c;
+
+    float d= 34.4f;
+    long double e = 34.4l;
+    cout<<"the size :"<<sizeof(3.4)<<endl;
+    cout<<"the size :"<<sizeof(3.4f)<<endl;
+    cout<<"the size :"<<sizeof(3.4F)<<endl;
+    cout<<"the size :"<<sizeof(3.4L)<<endl;
+    cout<<"the size :"<<sizeof(3.4l)<<endl; 
+    cout<<"the value of c:"<<d<<endl<<"the value of e is:"<<e<<endl;
+
     return 0;
 }
