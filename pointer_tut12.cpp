610313a09bba4f8766ca982499c3de15892afcd2
Himanshu Yadav <iamhimanshu1926>
2024-07-31 22:16:44 +0530

    addd

---
+++ pointer_tut12.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int a=23;
+int* b = &a ;
+
+cout<<"the address of a is "<<&a<<endl;
+cout<<"the address of b is "<<b<<endl;
+cout<<"the address of a is "<<*b<<endl;
+
+    return 0;
+}
