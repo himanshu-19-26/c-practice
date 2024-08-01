e36aedbcc3f94ed8da6b020f8854477d6c09ce93
Himanshu Yadav <iamhimanshu1926>
2024-08-01 19:53:18 +0530

    added

--- pointer_tut12.cpp
+++ pointer_tut12.cpp
@@ -6,8 +6,15 @@ int a=23;
 int* b = &a ;
 
 cout<<"the address of a is "<<&a<<endl;
-cout<<"the address of b is "<<b<<endl;
-cout<<"the address of a is "<<*b<<endl;
+cout<<"the address of a is "<<b<<endl;
+cout<<"the value of b is "<<*b<<endl;
+
+int **c=&b;
+cout<<"the adress of b "<<&b<<endl;
+cout<<"the adress of b "<<c<<endl;
+cout<<"the value of at adress c "<<*c<<endl;
+cout<<"the value of at adress of adress c "<<**c<<endl;
+
 
     return 0;
 }
